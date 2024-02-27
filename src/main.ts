516e873c59618a3e66d450d6e543510b4c1507f7
Yu Hang Hu <yuhang.hu@student.pxl.be>
2024-02-27 15:28:50 +0100

    Finished routing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import { router } from '@/router/router'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
---
+++ src/modules/auth/views/AuthLoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/modules/auth/stores/auth.store'
+
+const store = useAuthStore()
+
+async function login(): Promise<void> {
+	await store.login({ username: 'yuhang@yuhang.be', password: 'string' })
+}
+</script>
+
+<template>
+	<h2>login</h2>
+	<button @click="login" />
+</template>
+
+<style scoped></style>
