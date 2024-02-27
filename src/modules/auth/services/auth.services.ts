6f89d53d3ced9904f73a4dd042947142a00e5078
Yu Hang Hu <92795003+YuHangHuPXL@users.noreply.github.com>
2024-02-27 01:34:30 +0100

    Finished Http client

--- src/modules/auth/services/auth.services.ts
+++ src/modules/auth/services/auth.services.ts
@@ -1,27 +1,42 @@
 /*eslint-disable camelcase*/
 
 import httpClient from '@/http/httpClient'
-import type { CurrentUser } from '@/models/user/currentUser/currentUser.model'
+import type { AuthToken } from '@/models/auth/authToken'
+import type { User } from '@/models/user/user.model'
+
 interface AuthService {
-	login: (username: string, password: string) => Promise<void>
-	getCurrentUser: () => Promise<CurrentUser>
+	login: (username: string, password: string) => Promise<AuthToken>
+	getCurrentUser: () => Promise<User>
 }
+
+function encodeQueryData(data: Record<string, string>): URLSearchParams {
+	const params = new URLSearchParams()
+
+	for (const [key, value] of Object.entries(data)) {
+		params.append(key, value)
+	}
+
+	return params
+}
+
 export const authService: AuthService = {
-	login: async (username: string, password: string): Promise<void> => {
-		const formData = {
+	login: async (username: string, password: string): Promise<AuthToken> => {
+		const formData = encodeQueryData({
 			client_id: import.meta.env.VITE_CLIENT_ID,
 			client_secret: import.meta.env.VITE_CLIENT_SECRET,
 			grant_type: 'password',
 			password: password,
 			username: username,
-		}
+		})
 
 		const config = {
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 		}
+
 		const response = await httpClient.post('/auth/token', formData, config)
+		return response.data
 	},
-	getCurrentUser: async (): Promise<CurrentUser> => {
+	getCurrentUser: async (): Promise<User> => {
 		const response = await httpClient.get('/users/me')
 		return response.data
 	},
