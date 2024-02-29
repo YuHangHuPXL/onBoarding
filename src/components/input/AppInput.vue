58c07b82bff2ba86889383aaeec9c6a8297e0c2a
Yu Hang Hu <92795003+YuHangHuPXL@users.noreply.github.com>
2024-02-29 04:21:39 +0100

    Implemented formango

--- src/components/input/AppInput.vue
+++ src/components/input/AppInput.vue
@@ -1,26 +1,45 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 
-defineProps<{
-	placeholder: string
-	inputType: string
-}>()
+withDefaults(
+	defineProps<{
+		placeholder: string
+		inputType: string
+		isValid: boolean
+		isDirty: boolean
+	}>(),
+	{
+		placeholder: 'shared.app_text_placeholder',
+		inputType: 'text',
+		isValid: false,
+		isDirt: false,
+	}
+)
+
 const emits = defineEmits<{
 	update: [value: string]
 }>()
 
 const input = ref<string>('')
+const isFocussed = ref<boolean>(false)
 
 watch(input, (newValue) => {
 	emits('update', newValue)
 })
+
+function toggleHasBlurred(): void {
+	isFocussed.value = !isFocussed.value
+}
 </script>
 
 <template>
 	<input
 		v-model="input"
-		class="invalid: h-12 w-full rounded-lg bg-light-periWinkel p-3"
+		class="h-12 w-full rounded-lg bg-light-periWinkel p-3"
+		:class="{ 'border border-red-500 outline-none': !isValid && isDirty && !isFocussed }"
 		:placeholder="placeholder"
 		:type="inputType"
+		@focusin="toggleHasBlurred"
+		@focusout="toggleHasBlurred"
 	/>
 </template>
